db7149c4fc4f4e0c40efe2e7cf58a8ecc98424a7
Dennis Hu <ddh241@gmail.com>
2020-05-28 18:10:45 -0400

    initial commit

---
+++ nullify_empty.php
@@ -0,0 +1,23 @@
+<?php
+/*
+   Plugin Name: Nullify Fields
+   Plugin URI: http://my-awesomeness-emporium.com
+   description: >- Prevents WordPress from returning `false` for 
+      empty fields instead of empty arrays or null. Customized for AAIFF Website.
+      Based off of the comment: https://github.com/gatsbyjs/gatsby/issues/4461
+   Version: 1.0
+   Author: Dennis Hu
+   License: GPL2
+   */
+
+function nullify_empty($value, $post_id, $field)
+{
+   if (empty($value)) {
+      return null;
+   }
+
+   return $value;
+}
+
+add_filter('acf/format_value/type=image', 'nullify_empty', 100, 3);
+add_filter('acf/format_value/type=relationship', 'nullify_empty', 100, 3);
